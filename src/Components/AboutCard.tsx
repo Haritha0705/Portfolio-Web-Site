64c48d6958ed6b5412f1f47fe9d399f9cf0b22a4
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-03 19:12:51 +0530

    add new chages

--- src/Components/AboutCard.tsx
+++ src/Components/AboutCard.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Code } from "lucide-react";
 
 const AboutCard:React.FC = () => {
     return (
-        <div>
-
+        <div className={'flex flex-col max-w-sm bg-gray-200 items-start py-12 px-10 gap-5'}>
+            <Code color={'blue'} size={40}/>
+            <h4 className={'text-black dark:text-white text-xl font-semibold'}>My Approach</h4>
+            <p className={'text-black dark:text-white text-lg'}>I believe in clean, readable code and user-centered design.My goal is to create applications that are not only functional but also intuitive and enjoyable to use.I'm passionate about web standards, accessibility, and performance optimization.</p>
         </div>
     );
 };
--- src/Sections/AboutPage.tsx
+++ src/Sections/AboutPage.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import Title from "../Components/Title.tsx";
 import AboutCard from "../Components/AboutCard.tsx";
 
+
 const AboutPage:React.FC = () => {
     return (
-        <section className="w-full min-h-screen pt-[80px] flex flex-col-reverse md:flex-row items-center justify-center gap-10 px-4 sm:px-6 lg:px-10 py-12 bg-white dark:bg-black transition-colors">
+        <section className="w-full min-h-screen  pt-[80px] flex flex-col  gap-10 px-4  bg-white dark:bg-black transition-colors">
             <Title title={'About Me'}/>
-            <div>
+
+            <div className={'flex justify-evenly'}>
                 <AboutCard/>
                 <AboutCard/>
                 <AboutCard/>
+            </div>
+            <div className={'flex justify-center items-start'}>
+                <p></p>
             </div>
         </section>
     );
