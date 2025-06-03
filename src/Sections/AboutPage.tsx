cc1c1ce8bdeff8fc4a385874615ece3030c51300
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-03 18:38:35 +0530

    create about section

--- src/Sections/AboutPage.tsx
+++ src/Sections/AboutPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutPage:React.FC = () => {
     return (
-        <section className="p-10">
+        <section className="w-full min-h-screen pt-[80px] flex flex-col-reverse md:flex-row items-center justify-center gap-10 px-4 sm:px-6 lg:px-10 py-12 bg-white dark:bg-black transition-colors">
             <h2 className="text-3xl font-semibold">About Me</h2>
             <p className="text-gray-700 dark:text-gray-300">
                 This section also adapts to light/dark theme.
