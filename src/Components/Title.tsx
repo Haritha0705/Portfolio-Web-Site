4ccbe8fd748aac41e9022152395e81ee881eb07f
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-03 18:53:00 +0530

    add new chages

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+interface titleProps {
+    title:string;
+}
 
-const Title:React.FC = () => {
+const Title:React.FC<titleProps> = ({title}) => {
     return (
-        <div>
-
+        <div className={'relative flex items-center justify-center'}>
+            <div className={'absolute bottom-[-20px] h-[0.3rem] w-1/2 bg-blue-500 rounded-2xl'}></div>
+            <h1 className={'text-4xl font-bold text-black dark:text-white'}>{title}</h1>
         </div>
     );
 };
--- src/Sections/AboutPage.tsx
+++ src/Sections/AboutPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Title from "../Components/Title.tsx";
+import AboutCard from "../Components/AboutCard.tsx";
 
 const AboutPage:React.FC = () => {
     return (
         <section className="w-full min-h-screen pt-[80px] flex flex-col-reverse md:flex-row items-center justify-center gap-10 px-4 sm:px-6 lg:px-10 py-12 bg-white dark:bg-black transition-colors">
-            <h2 className="text-3xl font-semibold">About Me</h2>
-            <p className="text-gray-700 dark:text-gray-300">
-                This section also adapts to light/dark theme.
-            </p>
+            <Title title={'About Me'}/>
+            <div>
+                <AboutCard/>
+                <AboutCard/>
+                <AboutCard/>
+            </div>
         </section>
     );
 };
