2d2bfc4399b8a1e9a24858d8daa3a23b2faf47df
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-09 18:46:47 +0530

    GetInTouchPage.tsx commponet add colours

--- src/Sections/GetInTouchPage.tsx
+++ src/Sections/GetInTouchPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from "../Components/Title.tsx";
 import { LucideMail, Phone, LucideMapPin, Linkedin, Github, Twitter, Send } from "lucide-react";
 import Button from "../Components/Button.tsx";
+import {Link} from "react-router-dom";
 
 const GetInTouchPage: React.FC = () => {
     return (
@@ -9,7 +10,7 @@ const GetInTouchPage: React.FC = () => {
             {/* Title + Description */}
             <div className="max-w-3xl mx-auto text-center mb-12">
                 <Title title="Get In Touch" />
-                <p className="mt-2 text-gray-700 dark:text-gray-300 text-sm sm:text-base">
+                <p className="mt-2 text-gray-600 dark:text-gray-300 text-sm sm:text-base">
                     Have a project in mind or want to discuss potential opportunities? Feel free to reach out using the form or the contact details below.
                 </p>
             </div>
@@ -17,78 +18,78 @@ const GetInTouchPage: React.FC = () => {
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 max-w-6xl mx-auto">
                 {/* Contact Info */}
                 <div className="space-y-8">
-                    <h4 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Contact Information</h4>
+                    <h4 className="text-xl font-semibold text-black dark:text-white">Contact Information</h4>
 
                     <div className="flex items-start gap-4">
                         <div className="bg-blue-100 dark:bg-blue-900 p-3 rounded-full">
-                            <LucideMail className="text-blue-500 dark:text-blue-300" />
+                            <LucideMail className="text-blue-600 dark:text-blue-500" />
                         </div>
                         <div>
-                            <p className="text-gray-700 dark:text-gray-300 font-medium">Email</p>
+                            <p className="text-gray-600 dark:text-gray-300 font-medium">Email</p>
                             <span className="text-sm text-gray-600 dark:text-gray-400">harithawikramasinha2003@gmail.com</span>
                         </div>
                     </div>
 
                     <div className="flex items-start gap-4">
                         <div className="bg-blue-100 dark:bg-blue-900 p-3 rounded-full">
-                            <Phone className="text-blue-500 dark:text-blue-300" />
+                            <Phone className="text-blue-600 dark:text-blue-500" />
                         </div>
                         <div>
-                            <p className="text-gray-700 dark:text-gray-300 font-medium">Phone</p>
+                            <p className="text-gray-600 dark:text-gray-300 font-medium">Phone</p>
                             <span className="text-sm text-gray-600 dark:text-gray-400">+94 76 123 4567</span>
                         </div>
                     </div>
 
                     <div className="flex items-start gap-4">
                         <div className="bg-blue-100 dark:bg-blue-900 p-3 rounded-full">
-                            <LucideMapPin className="text-blue-500 dark:text-blue-300" />
+                            <LucideMapPin className="text-blue-600 dark:text-blue-500" />
                         </div>
                         <div>
-                            <p className="text-gray-700 dark:text-gray-300 font-medium">Location</p>
+                            <p className="text-gray-600 dark:text-gray-300 font-medium">Location</p>
                             <span className="text-sm text-gray-600 dark:text-gray-400">111, Katuwaththawala, Ibbawala, Weligama</span>
                         </div>
                     </div>
 
                     <h4 className="text-lg font-semibold text-gray-800 dark:text-gray-200 mt-6">Connect with Me</h4>
                     <div className="flex items-center gap-4">
-                        <a href="#" className="bg-gray-200 dark:bg-gray-700 p-3 rounded-full hover:scale-105 transition-transform">
-                            <Github className="text-black dark:text-white" />
-                        </a>
-                        <a href="#" className="bg-gray-200 dark:bg-gray-700 p-3 rounded-full hover:scale-105 transition-transform">
-                            <Linkedin className="text-black dark:text-white" />
-                        </a>
-                        <a href="#" className="bg-gray-200 dark:bg-gray-700 p-3 rounded-full hover:scale-105 transition-transform">
-                            <Twitter className="text-black dark:text-white" />
-                        </a>
+                        <Link to={'https://github.com/your-username'} target="_blank" rel="noopener noreferrer" className="bg-gray-100 dark:bg-gray-800 p-4 rounded-full hover:scale-105 transition-transform">
+                            <Github className="text-gray-600 dark:text-gray-300" />
+                        </Link>
+                        <Link to={'https://linkedin.com/in/your-username'} target="_blank" rel="noopener noreferrer" className="bg-gray-100 dark:bg-gray-800 p-4 rounded-full hover:scale-105 transition-transform">
+                            <Linkedin className="text-gray-600 dark:text-gray-300" />
+                        </Link>
+                        <Link to={'https://linkedin.com/in/your-username'} target="_blank" rel="noopener noreferrer" className="bg-gray-100 dark:bg-gray-800 p-4 rounded-full hover:scale-105 transition-transform">
+                            <Twitter className="text-gray-600 dark:text-gray-300" />
+                        </Link>
                     </div>
                 </div>
 
                 {/* Contact Form */}
                 <div className="space-y-6">
-                    <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Send Me a Message</h3>
+                    <h3 className="text-xl font-semibold text-black dark:text-white">Send Me a Message</h3>
 
                     <div className="space-y-4">
                         <div>
-                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">Your Name</label>
-                            <input type="text" className="w-full text-gray-700 dark:text-gray-300 px-4 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
+                            <label className="block text-sm font-medium text-gray-600 dark:text-gray-300">Your Name</label>
+                            <input type="text" className="w-full text-gray-600 dark:text-gray-400 px-4 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
                         </div>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">Your Email</label>
-                            <input type="email" className="w-full text-gray-700 dark:text-gray-300 px-4 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
+                            <label className="block text-sm font-medium text-gray-600 dark:text-gray-300">Your Email</label>
+                            <input type="email" className="w-full text-gray-600 dark:text-gray-400 px-4 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
                         </div>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">Subject</label>
-                            <input type="text" className="w-full text-gray-700 dark:text-gray-300 px-4 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
+                            <label className="block text-sm font-medium text-gray-600 dark:text-gray-300">Subject</label>
+                            <input type="text" className="w-full text-gray-600 dark:text-gray-400 px-4 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
                         </div>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">Message</label>
-                            <textarea rows={4} className="w-full px-4 text-gray-700 dark:text-gray-300 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
+                            <label className="block text-sm font-medium text-gray-600 dark:text-gray-300">Message</label>
+                            <textarea rows={4} className="w-full px-4 text-gray-600 dark:text-gray-400 py-2 mt-1 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600" />
                         </div>
                     </div>
 
                     <Button
                         text="Send Message"
-                        bgColor="bg-blue-500"
+                        bgColor="bg-blue-600 dark:bg-blue-500"
                         className="flex items-center gap-2 px-6 py-3 rounded-lg text-white dark:text-black hover:opacity-90 transition-all"
                         icon={<Send className="text-white dark:text-black"/>}
                     />
