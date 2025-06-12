32d35227d717589d746d24105bde78391642bc2b
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-12 19:20:32 +0530

    fix project card style issue

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -7,9 +7,10 @@ interface ProjectCardProps {
     description: string;
     image: any;
     category: string;
+    technology: string[];
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, image }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ technology ,title, description, image }) => {
     return (
 
         <motion.div
@@ -18,13 +19,16 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, image }) 
             transition={{duration:0.5}}
             whileHover={{scale:1,y:-5,boxShadow:'0px 10px 20px rgba(0,0,0,0.2)'}}
             className="w-full bg-gray-100 dark:bg-gray-800 rounded-2xl shadow-md overflow-hidden transition-colors hover:scale-105 hover:shadow-xl cursor-pointer">
-            <img src={image} alt={title} className="w-full h-48 object-cover" />
+            <img src={image} alt={title} className="w-full h-40 object-cover" />
             <div className="p-4 space-y-2">
                 <h5 className="text-lg font-semibold text-black dark:text-white">{title}</h5>
                 <p className="text-sm text-gray-600 dark:text-gray-300">{description}</p>
                 <div className="flex flex-wrap gap-2 mt-2">
-                    <span className="py-1 px-2 bg-gray-200 dark:bg-gray-700 rounded text-xs text-gray-600 dark:text-gray-400">React</span>
-                    <span className="py-1 px-2 bg-gray-200 dark:bg-gray-700 rounded text-xs text-gray-600 dark:text-gray-400">Node.js</span>
+                    {technology.map((tech, index) => (
+                        <span key={index} className="py-1 px-2 bg-gray-200 dark:bg-gray-700 rounded text-xs text-gray-600 dark:text-gray-400">
+                        {tech}
+                        </span>
+                    ))}
                 </div>
                 <div className="flex justify-between items-center pt-4 text-sm">
                     <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline flex items-center gap-1">
--- src/Sections/ProjectPage.tsx
+++ src/Sections/ProjectPage.tsx
@@ -10,45 +10,51 @@ import GrabIT from "../assets/GrabIT.png"
 const projects = [
     {
         id: 1,
-        title: "Etutor-lms",
+        title: "E-tutor-lms",
         description: "A full-featured e-commerce store with cart and Stripe payments.",
-        image: {Etutor},
+        image: Etutor,
+        technology: ["React","Node js"],
         category: "Web",
     },
     {
         id: 2,
         title: "Mobile Fitness App",
         description: "Track workouts, progress, and sync data across devices.",
-        image: {FitnessApp},
+        image: FitnessApp,
+        technology: ["Dart","Flutter"],
         category: "Mobile",
     },
     {
         id: 3,
         title: "Grab IT Mobile App UI ",
         description: "Redesigned a dashboard with modern UI/UX principles.",
-        image: {GrabIT},
+        image: GrabIT,
+        technology: ["Figma"],
         category: "UI/UX",
     },
     {
         id: 4,
         title: "Student-Management-System",
         description: "Content management system for writers and bloggers.",
+        technology: ["React","Node js"],
         image: "https://images.unsplash.com/photo-1611262588024-d12430b98920?auto=format&fit=crop&w=800&q=80",
         category: "Web",
     },
     {
         id: 5,
-        title: " library_management_system",
+        title: " Library Management System",
         description: "Mobile app to plan and organize travel experiences.",
+        technology: ["React","Node js"],
         image: "https://images.unsplash.com/photo-1524850301259-7729d41d11d9?q=80&w=3494&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        category: "Mobile",
+        category: "Web",
     },
     {
         id: 6,
         title: "Portfolio Website",
         description: "Personal portfolio to showcase my projects and skills.",
+        technology: ["React","Vite","TypeScript","Tailwind CSS","Motion","EmailJS"],
         image: "https://images.unsplash.com/photo-1642132652798-ae887edb9e9d?q=80&w=3520&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        category: "UI/UX",
+        category: "Web",
     },
 ];
 
@@ -132,6 +138,7 @@ const ProjectPage: React.FC = () => {
                             title={project.title}
                             description={project.description}
                             image={project.image}
+                            technology={project.technology}
                             category={project.category}
                         />
                     </motion.div>
