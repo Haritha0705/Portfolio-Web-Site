7dbf56c2cde6bce40790df761f97354b9ada7d01
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-12 19:25:11 +0530

    done in projectData.ts

--- src/Sections/ProjectPage.tsx
+++ src/Sections/ProjectPage.tsx
@@ -3,60 +3,8 @@ import Title from "../Components/Title.tsx";
 import ProjectCard from "../Components/ProjectCard.tsx";
 import Button from "../Components/Button.tsx";
 import { motion } from "framer-motion";
-import Etutor from "../assets/Homepage.jpg"
-import FitnessApp from "../assets/FitnessApp.png"
-import GrabIT from "../assets/GrabIT.png"
+import {projects} from "../data/projectData.ts"
 
-const projects = [
-    {
-        id: 1,
-        title: "E-tutor-lms",
-        description: "A full-featured e-commerce store with cart and Stripe payments.",
-        image: Etutor,
-        technology: ["React","Node js"],
-        category: "Web",
-    },
-    {
-        id: 2,
-        title: "Mobile Fitness App",
-        description: "Track workouts, progress, and sync data across devices.",
-        image: FitnessApp,
-        technology: ["Dart","Flutter"],
-        category: "Mobile",
-    },
-    {
-        id: 3,
-        title: "Grab IT Mobile App UI ",
-        description: "Redesigned a dashboard with modern UI/UX principles.",
-        image: GrabIT,
-        technology: ["Figma"],
-        category: "UI/UX",
-    },
-    {
-        id: 4,
-        title: "Student-Management-System",
-        description: "Content management system for writers and bloggers.",
-        technology: ["React","Node js"],
-        image: "https://images.unsplash.com/photo-1611262588024-d12430b98920?auto=format&fit=crop&w=800&q=80",
-        category: "Web",
-    },
-    {
-        id: 5,
-        title: " Library Management System",
-        description: "Mobile app to plan and organize travel experiences.",
-        technology: ["React","Node js"],
-        image: "https://images.unsplash.com/photo-1524850301259-7729d41d11d9?q=80&w=3494&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        category: "Web",
-    },
-    {
-        id: 6,
-        title: "Portfolio Website",
-        description: "Personal portfolio to showcase my projects and skills.",
-        technology: ["React","Vite","TypeScript","Tailwind CSS","Motion","EmailJS"],
-        image: "https://images.unsplash.com/photo-1642132652798-ae887edb9e9d?q=80&w=3520&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        category: "Web",
-    },
-];
 
 
 const categories = ["All", "Web", "Mobile", "UI/UX"];
